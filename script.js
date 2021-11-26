3547441e26e423ee3f685cbfb37338d7bbcd3cdd
Dipanshu Bhangale <dipu.bhangale@gmail.com>
2021-11-26 13:43:01 +0530

    fourth commit

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
 
 
+
+
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -17,8 +21,8 @@ btn.onclick = function() {
 }
 
 btn1.onclick = function() {
-    modal1.style.display = "block";
-  
+  modal1.style.display = "block";
+
 }
 
 btn2.onclick = function() {
@@ -26,18 +30,24 @@ btn2.onclick = function() {
 
 }
 
+
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
   modal1.style.display = "none";
   modal2.style.display = "none";
+
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal || event.target == modal1 ||event.target == modal2)  {
+  if (event.target == modal || event.target == modal1|| event.target == modal2)  {
     modal.style.display = "none";
     modal1.style.display = "none";
     modal2.style.display = "none";
+
+   
   }
 }
